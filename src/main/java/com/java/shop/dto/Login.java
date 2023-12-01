72da6d9449a731dfd3359f5fd629e11f12b79979
MarkJun <dnflskfk0200@naver.com>
2023-12-01 11:42:49 +0900

    backend

---
+++ src/main/java/com/java/shop/dto/Login.java
@@ -0,0 +1,24 @@
+package com.java.shop.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Login {
+    private int uCode;
+    private String uId;
+    private String uPw;
+    private String uNick;
+    private String uEMail;
+    private int uEmailCheck;
+    private String uPhone;
+    private String uGrade;
+    private Date uRegiDate;
+    private Date uLastDate;
+    private int uDrop;
+
+    private String uAccess;
+    
+    
+}
